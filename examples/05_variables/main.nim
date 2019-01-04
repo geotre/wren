0623c13dac01d6a1e90d8d1e4a5f478165c2b204
geotre <georgetrevill@gmail.com>
2019-01-04 21:48:24 +0000

    fix typo

--- examples/05_variables/main.nim
+++ examples/05_variables/main.nim
@@ -6,5 +6,5 @@ let vm = defaultConfig().newVM()
 vm.runScript("main.wren")
 
 vm.ensureSlots(1)
-vm.getVariable("main", "wrenVar", 0)  # gets main.testvar and puts it into slot 3
+vm.getVariable("main", "wrenVar", 0)  # gets main.wrenVar
 echo vm.getSlotString(0)
